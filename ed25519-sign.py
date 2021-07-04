ef117b06bcf3936e832b157f081ca3a78cff7b8d
zach <zachanon@gmail.com>
2021-07-04 15:30:47 -0500

    can hash things and even sign messages now

--- ed25519-sign.py
+++ ed25519-sign.py
@@ -1,22 +1,22 @@
+import argparse
+from sys import stdin
 from nacl import signing, encoding
 
-def sign(keyfile, messagefile):
+def sign(keyfile, message):
 
   with open(keyfile) as file:
     pk = file.readline().strip().encode('utf-8')
-  with open(messagefile) as file:
-    msg = file.readline().encode('utf-8')
-
+  
   encoder = encoding.HexEncoder
   signer = signing.SigningKey(pk, encoder=encoder)
   
-  signed = signer.sign(msg, encoder=encoder)
-  verify_key = signer.verify_key.encode(encoder=encoder)
+  if message:
+    msg = message.encode('utf-8')
+  else:
+    msg = stdin.read().encode('utf-8')
 
-  with open('vkey.pem', 'w') as file:
-    file.write(verify_key.decode())
-  with open('smsg.pem', 'w') as file:
-    file.write(signed.decode())
+  signed = signer.sign(msg, encoder=encoder)
+  print(signed.decode())
 
 def verify(verify_key, signed_message):
 
@@ -30,5 +30,10 @@ def verify(verify_key, signed_message):
   verifier = signing.VerifyKey(vkey, encoder=encoder)
   return verifier.verify(smsg, encoder=encoder)
 
-sign('key.pem', 'msg.pem')
-verify('vkey.pem', 'smsg.pem')
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument("keyfile")
+  parser.add_argument("--message")
+
+  args = parser.parse_args()
+  sign(args.keyfile, args.message)
