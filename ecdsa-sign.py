747503bac75acb0813df795cbbf364a065c4993c
zach <zachanon@gmail.com>
2021-07-03 21:41:25 -0500

    fixed script

--- ecdsa-sign.py
+++ ecdsa-sign.py
@@ -1,8 +1,8 @@
 #modified code from https://cryptobook.nakov.com/digital-signatures/ecdsa-sign-verify-examples to work with the current pycoin package
 
-from pycoin.ecdsa import sign, verify
-from pycoin.ecdsa.secp256k1 import secp256k1_generator
+from pycoin.ecdsa.secp256k1 import secp256k1_generator as generator
 import hashlib, secrets
+
 
 def sha3_256Hash(msg):
   hashBytes = hashlib.sha3_256(msg.encode("utf8")).digest()
@@ -10,24 +10,24 @@ def sha3_256Hash(msg):
 
 def signECDSAsecp256k1(msg, privKey):
   msgHash = sha3_256Hash(msg)
-  signature = sign(secp256k1_generator, privKey, msgHash)
+  signature = generator.sign(privKey, msgHash)
   return signature
 
 def verifyECDSAsecp256k1(msg, signature, pubKey):
   msgHash = sha3_256Hash(msg)
-  valid = verify(secp256k1_generator, pubKey, msgHash, signature)
+  valid = generator.verify(pubKey, msgHash, signature)
   return valid
 
 # ECDSA sign message (using the curve secp256k1 + SHA3-256)
 msg = "Message for ECDSA signing"
-privKey = secrets.randbelow(secp256k1_generator.order())
+privKey = secrets.randbelow(generator.order())
 signature = signECDSAsecp256k1(msg, privKey)
 print("Message:", msg)
 print("Private key:", hex(privKey))
 print("Signature: r=" + hex(signature[0]) + ", s=" + hex(signature[1]))
 
 # ECDSA verify signature (using the curve secp256k1 + SHA3-256)
-pubKey = (secp256k1_generator * privKey).pair()
+pubKey = generator.raw_mul(privKey)
 valid = verifyECDSAsecp256k1(msg, signature, pubKey)
 print("\nMessage:", msg)
 print("Public key: (" + hex(pubKey[0]) + ", " + hex(pubKey[1]) + ")")
